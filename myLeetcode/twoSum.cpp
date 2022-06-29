4d83a8eb51201f77f02f9fb037aec3e1782ed89e
Jacobson, Cade <cade.jacobson@mines.sdsmt.edu>
2022-06-28 22:21:18 -0500

    Fix overstepping vector bounds

--- myLeetcode/twoSum.cpp
+++ myLeetcode/twoSum.cpp
@@ -4,13 +4,13 @@ public:
         int i, j;
         vector<int> answer;
         bool answerFound = false;
-        for( i = 0; i < nums.size(), answerFound == false; i++ )
+        for( i = 0; i < nums.size() && answerFound == false; i++ )
         {
-            for(j = i + 1; j < nums.size(), answerFound == false; j++ )
-            {
+            for(j = i + 1; j < nums.size() && answerFound == false; j++ ) 
+            {                           //begin after i's position since the indices must be unique
                if( target - nums[j] == nums[i] )
                {
-                   answer.push_back(i);
+                   answer.push_back(i); //add the indices of the answers to the vector and return it
                    answer.push_back(j);
                    answerFound = true;
                }
